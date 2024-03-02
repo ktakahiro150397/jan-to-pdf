339700210bf5c6f50e4896316d5d380810d9bcd3
ktakahiro150397 <k_takahiro_150397@yahoo.co.jp>
2024-03-02 13:12:37 +0900

    データクラス追加

---
+++ barcode_data.py
@@ -0,0 +1,6 @@
+from dataclasses import dataclass
+
+@dataclass
+class barcodeData:
+    barcode_path: str
+    product_name: str
--- main.py
+++ main.py
@@ -2,26 +2,43 @@
 # -*- coding: utf-8 -*-
 
 import csv,barcode
+from barcode_data import barcodeData
 from barcode.writer import ImageWriter
 
 output_pic_dir = "output_pic"
 
-# csvの読み込み
-with open(u'barcode_base.csv', "r") as f:
 
-    # csvから内容を一括で取り出す
-    reader = csv.reader(f)
+def main():
 
-    # 1行ずつ取り出す
-    for row in reader:
-        # JANコード画像の生成
-        jan = barcode.get('jan', row[1], writer=ImageWriter())
-        # JANコード画像の保存
-        filename = jan.save(f"{output_pic_dir}/{row[0]}",options={
-            #'module_height':37.29,
-            #'module_width':25.93,
-            'quiet_zone':2,
-            #'font_size':40,
-            #'text_distance':5,
-            #'font_path':'C:\\users\\hogehoge\\appdata\\local\\microsoft\\windows\\fonts\\ocrb.ttf'
-            })
+    data = []
+
+    # csvの読み込み
+    with open(u'barcode_base.csv', "r") as f:
+
+        # csvから内容を一括で取り出す
+        reader = csv.reader(f)
+
+        # 1行ずつ取り出す
+        for row in reader:
+            barcode_path = f"{output_pic_dir}/{row[0]}.png"
+            product_name = row[1]
+
+            # JANコード画像の生成
+            jan = barcode.get('jan', product_name, writer=ImageWriter())
+            # JANコード画像の保存
+            filename = jan.save(f"{output_pic_dir}/{row[0]}",options={
+                #'module_height':37.29,
+                #'module_width':25.93,
+                'quiet_zone':2,
+                #'font_size':40,
+                #'text_distance':5,
+                #'font_path':'C:\\users\\hogehoge\\appdata\\local\\microsoft\\windows\\fonts\\ocrb.ttf'
+                })
+            data.append(barcodeData(barcode_path, product_name))
+
+    # データの表示
+    for d in data:
+        print(d.barcode_path, d.product_name)
+
+if __name__ == "__main__":
+    main()

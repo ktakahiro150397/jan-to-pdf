772f17814f8c46969c9112235ebe255647a6fba5
ktakahiro150397 <k_takahiro_150397@yahoo.co.jp>
2024-03-03 11:45:47 +0900

    商品JANコードPDF出力処理

--- make_pdf.py
+++ make_pdf.py
@@ -24,9 +24,22 @@ class MakePdf:
         pdfmetrics.registerFont(TTFont('meiryo', 'Meiryo.ttf'))
         page.setFont('meiryo', 12)
 
-        page.drawString(50, 800, "JANコード一覧")
+        # page.drawString(50, 800, "JANコード一覧")
 
-        for i, data in enumerate(self.barcode_data):
+        # 10個ずつバーコードを描画
+        for page_num, data in enumerate(self.split_list(self.barcode_data,10)):
+            self.draw_barcode_list(page,page_num,data)
+            page.showPage()
+        
+        page.save()
+
+        print(f"Output PDF to {self.file_path}")
+    
+    def draw_barcode_list(self,page:canvas.Canvas,page_num:int,barcode_data:List[barcodeData]):
+        page.setFont('meiryo', 12)
+        page.drawString(50, 800, f"JANコード一覧 {page_num+1}ページ")
+        
+        for i, data in enumerate(barcode_data):
             # 列位置
             if i <= BARCODE_CHANGE_DATA_COUNT - 1:
                 position_x = BARCODE_FIRST_COLUMN_X
@@ -42,10 +55,6 @@ class MakePdf:
             self.draw_barcode(page,data,position_x,position_y)
             print(f"index:{i} Output {data.product_name} to PDF ({position_x},{position_y})")
 
-        page.save()
-
-        print(f"Output PDF to {self.file_path}")
-
     def draw_barcode(self,page:canvas.Canvas,barcode_data:barcodeData,position_x,position_y):
         font_size = 12
         page.setFont('meiryo', font_size)
@@ -54,3 +63,7 @@ class MakePdf:
         page.drawString(x=position_x,y=position_y-font_size*1.2,text=barcode_data.product_name)
         page.drawImage(barcode_data.barcode_path,position_x,position_y-font_size*1.2-105,
                        width=200,height=100)
+        
+    def split_list(self,barcode_data:List[barcodeData],n:int):
+        for idx in range(0,len(barcode_data),n):
+            yield barcode_data[idx:idx+n]
